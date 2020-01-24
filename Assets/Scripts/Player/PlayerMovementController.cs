fc74f6a87c311e5e2e9626b741f9b8a55a25f0a2
Braden Walters <vc@braden-walters.info>
2020-01-24 20:37:09 +0100

    Fixed problems in input settings; Prefab for player movement controller and script that prints action occurrences

---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementController : MonoBehaviour {
+  public enum ActivePlayer {
+    Player1,
+    Player2
+  }
+
+  public ActivePlayer activePlayer = ActivePlayer.Player1;
+
+  public void OnMove(InputValue value) {
+    Debug.Log("Move " + value.Get<Vector2>());
+  }
+
+  public void OnToggleActive(InputValue value) {
+    Debug.Log("Toggle");
+  }
+
+  public void OnRotateCamera(InputValue value) {
+    Debug.Log("Rotate " + value.Get<float>());
+  }
+}
